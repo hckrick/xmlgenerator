e3b94db4765f59c01175983ce3bed57f29eaee59
Hck_Rick <32933771+hckrick@users.noreply.github.com>
2023-03-20 09:33:39 -0400

    Add files via upload

---
+++ src/Interface/DocumentBuilder.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interface;
+
+import javax.swing.text.Document;
+
+/**
+ *
+ * @author samsepiol
+ */
+class DocumentBuilder {
+
+    Document newDocument() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/Interface/sheet.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interface;
+
+import jxl.Cell;
+
+/**
+ *
+ * @author samsepiol
+ */
+class sheet {
+
+    static int getRows() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Cell getCell(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
